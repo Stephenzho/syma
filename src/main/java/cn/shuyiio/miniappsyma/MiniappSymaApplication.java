3cb21309ff167a73a8e7c8b201e3719b89bd84cf
stephenzhou <stephen_zho@126.com>
2019-08-12 22:30:11 +0800

    fix folder

---
+++ src/main/java/cn/shuyiio/miniappsyma/MiniappSymaApplication.java
@@ -0,0 +1,13 @@
+package cn.shuyiio.miniappsyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniappSymaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniappSymaApplication.class, args);
+	}
+
+}
