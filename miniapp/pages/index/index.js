06e2aeb2a2b3d29a97ce628d5249c2f8f4321223
zhoushuyi <stephen_zho@126.com>
2019-08-23 21:09:18 +0800

    demo

--- miniapp/pages/index/index.js
+++ miniapp/pages/index/index.js
@@ -4,16 +4,16 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
+      //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+        wx.navigateTo({
+          url: '../logs/logs'
+        })
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -43,7 +43,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  setUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
